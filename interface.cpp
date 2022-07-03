35d4bb4b3f98eb7e0251f7e7137be14a4c1b7c48
TheDannified <90780584+TheDannified@users.noreply.github.com>
2022-07-03 16:15:06 +0800

    updated uiEasyMode (and uiMediumMode?)
    mas malinis na yung uiEasyMode. 😅 yung sa may uiMediumMode tho... 👀💦

--- interface.cpp
+++ interface.cpp
@@ -10,15 +10,10 @@ void uiMainMenu() {
     cout << endl << "COMMAND: ";
 }
 
+// IN DEVELOPMENT
 void uiEasyMode() {
-    bool isInGame = true;   // Determines if the game is ongoing
     Player user;    // User player object.
     Player comp;    // Computer player object.
-
-    Code userGuess;     // Stores the most recent guess code of the "user" player
-    Code compGuess;     // Stores the most recent guess code of the "comp" player
-
-    map<string, int> result;    // guess evaluate tracker.
 
     // Prompt user to provide their own secret code.
     promptSecretCode(user);
@@ -27,93 +22,71 @@ void uiEasyMode() {
     comp.secretCode = generateCode(EASY, comp);
     cout << "[COMPUTER] Computer has generated its own secret code." << endl << endl;
 
-    cout << "************** COMMENCE PLAYGROUND **************" << endl << endl;
-    do {
-        // GAME HEADER/COUNTER
-        cout << "******************** GAME #" << user.guesses.size() + 1 << " ********************" << endl;
+    initGame(user, comp, MANUAL);
 
-        // Print all guesses made by players first.
-        cout << "YOUR GUESSES: ";
-        for (const Code& usrGuess : user.guesses)
-            cout << usrGuess << " ";
-        cout << endl;
-        cout << "COMP GUESSES: ";
-        for (const Code& cmpGuess : comp.guesses)
-            cout << cmpGuess << " ";
-        cout << endl;
-        cout << "*************************************************" << endl;
-
-        // Prompt the user to guess the computer's secret code.
-        promptGuessCode(user);
-        userGuess = user.guesses.at(user.guesses.size() - 1);
-        result = evaluateCode(userGuess, comp.secretCode);
-        cout << "USER'S RESULT: " << printResult(result) << endl;
-
-        // Generate a code for the computer.
-        compGuess = generateCode(EASY, comp);
-        comp.guesses.push_back(compGuess);
-        result = evaluateCode(compGuess, user.secretCode);
-        cout << "COMPUTER GUESS: " << compGuess << endl;
-        cout << "COMPUTER'S RESULT: " << printResult(result) << endl;
-
-        cout << "*************************************************" << endl;
-
-        // CHECK IF THE USER AND COMPUTER WERE ABLE TO GUESS EACH OTHER'S SECRET CODE
-        if (hasGuessedSecretCode(userGuess, comp.secretCode) && hasGuessedSecretCode(compGuess, user.secretCode))
-        {
-            cout << "***************** GAME  SUMMARY *****************" << endl;
-            cout << "[GAME] It's a draw! No one guessed each other's secret code!"  << endl;
-            cout << "User's Last Guess: " << userGuess << endl;
-            cout << "Computer's Secret Code: " << comp.secretCode << endl << endl;
-            cout << "Computer's Last Guess: " << compGuess << endl;
-            cout << "User's Secret Code: " << user.secretCode << endl;
-            cout << "*************************************************" << endl;
-        }
-        // CHECK IF THE USER WAS THE FIRST TO GUESS THE COMPUTER'S SECRET CODE
-        else if (hasGuessedSecretCode(userGuess, comp.secretCode))
-        {
-            user.isWinner = true;
-            comp.isWinner = false;
-            isInGame = false;
-
-            cout << "***************** GAME  SUMMARY *****************" << endl;
-            cout << "[GAME] The USER has guessed the COMPUTER'S secret code!"  << endl;
-            cout << "User's Last Guess: " << userGuess << endl;
-            cout << "Computer's Secret Code: " << comp.secretCode << endl;
-            cout << "*************************************************" << endl;
-        }
-        // CHECK IF THE COMPUTER WAS THE FIRST TO GUESS THE USER'S SECRET CODE
-        else if (hasGuessedSecretCode(compGuess, user.secretCode))
-        {
-            comp.isWinner = true;
-            user.isWinner = false;
-            isInGame = false;
-
-            cout << "***************** GAME  SUMMARY *****************" << endl;
-            cout << "[GAME] The COMPUTER has guessed the USER'S secret code!"  << endl;
-            cout << "Computer's Last Guess: " << userGuess << endl;
-            cout << "User's Secret Code: " << comp.secretCode << endl;
-            cout << "*************************************************" << endl;
-        }
-
-        // CHECK IF THE USER HAS NOT EXHAUSTED THEIR ATTEMPTS YET AND THE GAME IS STILL ONGOING
-    } while (user.guesses.size() != MAX_GUESS_ATTEMPTS && isInGame);
-
-    // IF NONE OF THE PLAYERS WERE ABLE TO GUESS ONE ANOTHER'S CODE...
-    if (!user.isWinner && !comp.isWinner)
-    {
-        cout << "***************** GAME  SUMMARY *****************" << endl;
-        cout << "[GAME] It's a draw! No one guessed each other's secret code!"  << endl;
-        cout << "User's Last Guess: " << userGuess << endl;
-        cout << "Computer's Secret Code: " << comp.secretCode << endl << endl;
-        cout << "Computer's Last Guess: " << compGuess << endl;
-        cout << "User's Secret Code: " << user.secretCode << endl;
-        cout << "*************************************************" << endl;
-    }
 }
 
-
+// IN DEVELOPMENT
 void uiMediumMode() {
-    Player user;
-    Player comp;
+    fstream usrGuessesFile;     // fstream object that is responsible for handling our external (text) file
+    stringstream UGF_fileName;  // stringstream object that stores the name of the usrGuessesFile (UGF)
+
+    bool inAskingMode = true;   // Determines if the game system is asking for pre-made game settings
+
+    Player user;                // User player object
+    Player comp;                // Computer player object
+
+    char modeOfPlay;            // Stores the game setting of "how to play the medium game mode"
+
+    // Prompt user to provide their own secret code first.
+    promptSecretCode(user);
+
+    // Generate a code for the computer.
+    comp.secretCode = generateCode(EASY, comp);
+    cout << "[COMPUTER] Computer has generated its own secret code." << endl << endl;
+
+    do {
+        // PROMPT USER IF THEY WANT TO PLAY THE GAME:
+        // (1) BY INPUTTING GUESS CODES MANUALLY; OR
+        // (2) BY PROVIDING A PRE-SUPPLIED GUESS CODES FROM A TEXT FILE
+        cout << "[GAME] How would you like to play this game?" << endl;
+        cout << "[X] Let me provide guess codes manually." << endl;
+        cout << "[C] Let me provide guess codes from a text file." << endl;
+        cout << "[ARGUMENT] >> ";
+        cin >> modeOfPlay;
+
+        switch (toupper(modeOfPlay)) {
+            case MANUAL:
+                inAskingMode = false;
+                break;
+            case AUTOMATIC:
+                do {
+                    string tmpStr; // only a temporary string
+
+                    // PROMPT USER TO PROVIDE THE NAME OF THEIR TEXT FILE THAT CONTAINS THEIR GUESS CODE
+                    cout << "[GAME] Please provide the name (or path) of your text file that contains your guess code: ";
+                    cin >> tmpStr;
+                    UGF_fileName.str(tmpStr);
+
+                    usrGuessesFile.open(UGF_fileName.str(), fstream::in);
+
+                    if (usrGuessesFile.is_open() && !usrGuessesFile.fail())
+                    {
+                        while (getline(UGF_fileName, tmpStr, '\\'));
+
+                        cout << "[GAME] " << tmpStr << " has been loaded to the game system successfully." << endl << endl;
+                        inAskingMode = false;
+                    }
+
+                } while (inAskingMode);
+                break;
+            default:
+                cerr << "[ERROR] Invalid argument! Please try again." << endl;
+        }
+
+    } while (inAskingMode);
+
+    initGame(user, comp, (ModeOfPlay)modeOfPlay);
+
+    usrGuessesFile.close();
 }
