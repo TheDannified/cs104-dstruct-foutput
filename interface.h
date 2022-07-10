73f0b55c681eca9dc1ece47ad5a5b73c05040a23
TheDannified <2019-15226@sanbeda.edu.ph>
2022-07-11 00:55:08 +0800

    Added comments of UI functions
    Nakalimutan kong maglagay ng mga comments. 💀💀💀

--- interface.h
+++ interface.h
@@ -10,11 +10,34 @@
 
 using namespace std;
 
-void uiMainMenu();
-void uiEasyMode();
-void uiMediumMode();
 /**
- * uiPracticeMode
+ * uiMainMenu is a UI function that presents our users some available game options.
+ * This function is always invoked when the program boots up and every after a game
+ * is finished.
+ */
+void uiMainMenu();
+
+/**
+ * uiEasyMode, when invoked, enters the players to a game state where they will compete
+ * with an AI on Easy difficulty. In this game mode, the player and computer must guess
+ * each other's secret code. Whoever guesses a player's secret code first will win the
+ * current game.
+ */
+void uiEasyMode();
+
+/**
+ * uiEasyMode, when invoked, enters the players to a game state where they will compete
+ * with an AI on Medium difficulty. Just like the Easy difficulty, the player and computer
+ * must compete in guessing each other's secret code. However, this time, the computer is
+ * equipped with an extra step to guess its opponent's secret code. Gameplay is relatively
+ * the same with Easy difficulty.
+ */
+void uiMediumMode();
+
+/**
+ * uiPracticeMode, when invoked, enters the players to a practice game state where they
+ * can practice how the game works and test their guessing skills. In this game mode,
+ * only human players can guess a computer-generated secret code.
  */
 void uiPracticeMode();
 
